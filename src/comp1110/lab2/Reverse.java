83fac15a5912654345a1fcb9e0e78242f1994e1d
Manindra de Mel <44416444+ManindraDeMel@users.noreply.github.com>
2022-08-01 13:02:13 +1000

    Finished all compulsory lab 2 exercises

--- src/comp1110/lab2/Reverse.java
+++ src/comp1110/lab2/Reverse.java
@@ -3,7 +3,7 @@ package comp1110.lab2;
 public class Reverse {
     public static void main(String[] args) {
         int n = 30;
-        while (n >= 10) { // a do while will check for n first before starting the while loop. However, in this case it wouldn't change the code (problem solved the same way)
+        while (n >= 10) { // a do while will run the codeblock first so before checking for n>=10 it will decrement n, therefore the condition will need to change to n>10 instead of n>=10
             System.out.println(n);
             n--;
         }
